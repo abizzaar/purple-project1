662d334c6c745faee1daa1a8b3339c114f47c968
Gabe Rojas-Westall <rojaswestall@gmail.com>
2018-10-14 18:30:53 -0500

    changed to not use env var

--- backend/secrets.js
+++ backend/secrets.js
@@ -1,7 +1,10 @@
 // secrets.js
 const secrets = {
-//  dbUri: mongodb://purple_project:123456qwerty@ds125912.mlab.com:25912/purple_project
-	dbUri: process.env.DB_URI
+ dbUri: "mongodb://purple_project:123456qwerty@ds125912.mlab.com:25912/purple_project"
+	// dbUri: process.env.DB_URI
 };
 
 export const getSecret = key => secrets[key];
+
+// To use environment variables:
+// export DB_URI=mongodb://purple_project:123456qwerty@ds125912.mlab.com:25912/purple_project
