0686968391d8ed32a0ee32fc21fa31a6eecdf9eb
Gabe Rojas-Westall <rojaswestall@gmail.com>
2018-10-14 18:22:44 -0500

    instructions for curl command

--- backend/example-curl-command.sh
+++ backend/example-curl-command.sh
@@ -4,3 +4,9 @@ curl -H "Content-Type: application/json" --data \
       languages: ["en"],
       requestedAttributes: {TOXICITY:{}} }' \
     https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze?key=AIzaSyBwW-rzM_A6MF2G-mrGQixet1l55wzDabI
+
+# To create the executable run:
+# chmod +x example-curl-command.sh
+
+# To run the executable
+# ./example-curl-command.sh
