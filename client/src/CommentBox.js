fdf5ffc374c1040cd239a1fa4831381bd8a858c0
mayli10 <mali@vassar.edu>
2018-10-14 19:03:50 -0500

    added error message for toxicity & comment submission validation

--- client/src/CommentBox.js
+++ client/src/CommentBox.js
@@ -58,7 +58,8 @@ class CommentBox extends Component {
   submitComment = (e) => {
     e.preventDefault();
     const { author, text, updateId } = this.state;
-    if (!author || !text) return;
+    if (!author || !text) return window.confirm("Oops! You must've forgotten your name and comment!");
+    if (this.state.toxicity === 0.6) return window.confirm("Please revise your comment to decrease toxicity. We want to keep conversations productive and toxic-free. Happy PureChatting!");
     if (updateId) {
       this.submitUpdatedComment();
     } else {
